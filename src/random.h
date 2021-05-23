a747739f9168a985580caa650e903b9cc436c5ed
John Warburton <john@johnwarburton.net>
2021-05-23 18:30:22 +0100

    Try fixing type errors

--- src/random.h
+++ src/random.h
@@ -1,6 +1,8 @@
 /* These are in <stdlib.h> but not in MinGW-w64 among other implementations */
 
-char *setstate(const char *);
+#include <stddef.h>
+
+char *setstate(char *);
 char *initstate(unsigned int, char *, size_t);
 long random(void);
 void srandom(unsigned);
